14e00a26d0e1dd5a9c378f7abef391d3cd1aea22
Manuel Prieto <m.prietoh@uniandes.edu.co>
2024-05-17 15:05:31 -0500

    cambios

--- src/app/pinguino/pinguino-list/pinguino-list.component.ts
+++ src/app/pinguino/pinguino-list/pinguino-list.component.ts
@@ -10,7 +10,7 @@ import { PinguinoService } from '../pinguino.service';
 export class PinguinoListComponent implements OnInit {
 
   pinguinos: Array<Pinguino> = []; 
-  masDistribucion: string = "";
+  masPresencia: string = "";
   selected: Boolean = false;
   selectedPinguino!: Pinguino;
   
@@ -31,16 +31,31 @@ export class PinguinoListComponent implements OnInit {
   getPinguinoMasPresenciaMundial(): void {
     this.pinguinoService.getPinguinos().subscribe((pinguinos) => {
       let max = 0;
-      let pinguinoMasDistribucion = "";
+      let Hash = new Map<string, Map<string, number>>();
+
       for (let pinguino of pinguinos) {
-        if (pinguino.global_distribution.length > max) {
-          max = pinguino.global_distribution.length;
-          pinguinoMasDistribucion = pinguino.name;
+        if (Hash.has(pinguino.global_distribution)) {
+          let value = Hash.get(pinguino.global_distribution);
+          if (value) {
+            value.set(pinguino.name, (value.get(pinguino.name) || 0) + 1);
+          }
+        } else {
+          let value = new Map<string, number>();
+          value.set(pinguino.name, 1);
+          Hash.set(pinguino.global_distribution, value);
+        }
+      } 
+      for (let [key, value] of Hash) {
+        for (let [key2, value2] of value) {
+          if (value2 > max) {
+            max = value2;
+            this.masPresencia = key2;
+          }
         }
       }
-      this.masDistribucion = pinguinoMasDistribucion;
-    }
-    );
+    });
+
+      
     
   }
 
